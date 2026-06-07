fb9aa6517ffcffd00f21700705b3b35994507a3e
kumfort <zlosnik.dv00@bk.ru>
2026-06-07 22:36:05 +0300

    little more

--- src/homeworkNumber3/Main.java
+++ src/homeworkNumber3/Main.java
@@ -6,29 +6,38 @@ import java.util.Scanner;
 public class Main {
 
     private static final String sep = "-".repeat(50);
+    private static final long millis = 1000;
+
+    public enum Temperature {
+        WARM,
+        COLD,
+        NORMAL
+    }
 
     public static void main(String[] args) throws InterruptedException {
         fullOutputData();
     }
 
     public static String taskNum1(int num) {
-        return (num % 2 == 0) ? "число четное" : "число не четное";
+        return (num % 2 == 0) ? "число четное" : "число нечетное";
     }
 
-    public static String taskNum2(int temperature) {
+    public static Temperature taskNum2(int temperature) {
         if (temperature > -5) {
-            return "Warm";
+            return Temperature.WARM;
         } else if (temperature <= -20) {
-            return "Cold";
+            return Temperature.COLD;
         } else {
-            return "Normal";
+            return Temperature.NORMAL;
         }
     }
 
     public static int[] taskNum3() {
         int[] numbers = new int[11];
+        int num;
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = (10 + i) * (10 + i);
+            num = 10 + i;
+            numbers[i] = num * num;
         }
         return numbers;
     }
@@ -45,20 +54,38 @@ public class Main {
     }
 
     public static int taskNum5(int num) {
-        return (int) ((num - 1) * ((double) num / 2));
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+           if(num > 1){
+               scanner.close();
+               return (int) ((num - 1) * ((double) num / 2));
+           }
+           else {
+               System.out.println("Число должно быть больше 1, повторите попытку");
+               num = readInt(scanner);
+           }
+        }
+
+
+
     }
 
-    public static int exception(Scanner scanner) {
+    public static int readInt(Scanner scanner) {
         int num;
         while (true) {
             if (scanner.hasNextInt()) {
                 num = scanner.nextInt();
                 break;
             } else {
-                System.out.println("Ошибка! Введите целое число.");
+                System.out.println("Ошибка! Введите правильное целое число.");
                 scanner.next();
+
             }
         }
+
+
+
+
         return num;
     }
 
@@ -66,7 +93,7 @@ public class Main {
         int num = 0;
         Scanner scanner = new Scanner(System.in);
         for (int i = 1; i <= 5 ; i++) {
-            Thread.sleep(1500);
+            Thread.sleep(millis);
             if(i != 3 && i != 4){
                 System.out.printf("задание номер %d:\nВведите целое число " +
                         "для задания номер %d\n", i, i);
@@ -75,7 +102,7 @@ public class Main {
             }
 
             if(i != 3 && i != 4){
-                num = exception(scanner);
+                num = readInt(scanner);
             }
 
 
