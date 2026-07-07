86d2c649d9079c8f6a1ee9f4948f46a1db5748d1
kumfort <zlosnik.dv00@bk.ru>
2026-07-07 18:39:31 +0300

    game2

---
+++ src/GameTicTacToe/Main.java
@@ -0,0 +1,20 @@
+package GameTicTacToe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            Game game = new Game();
+            game.run();
+            System.out.println("Хотите начать заново: да/нет");
+            String answer = scanner.nextLine();
+            if(answer.equalsIgnoreCase("нет")){
+                System.out.println("Прощайте, до свидания");
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
