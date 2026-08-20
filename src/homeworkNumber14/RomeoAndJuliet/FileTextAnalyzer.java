e4712be16a27c912d0ddedfebde1ba77e8095298
kumfort <zlosnik.dv00@bk.ru>
2026-08-20 15:24:13 +0300

    fix

--- src/homeworkNumber14/RomeoAndJuliet/FileTextAnalyzer.java
+++ src/homeworkNumber14/RomeoAndJuliet/FileTextAnalyzer.java
@@ -2,15 +2,23 @@ package homeworkNumber14.RomeoAndJuliet;
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileTextAnalyzer {
     public void readAndWrite() {
         String maxLengthWord = "";
         try(Scanner scanner = new Scanner(new File("src/homeworkNumber14/RomeoAndJuliet/RomeoAndJuliet.txt"));
             FileOutputStream fileOutputStream = new FileOutputStream("src/homeworkNumber14/RomeoAndJuliet/Result.txt")) {
+            StringBuilder stringBuilder = new StringBuilder();
+            while (scanner.hasNextLine()) {
+                stringBuilder.append(scanner.nextLine()).append(" ");
+            }
             String word;
-            while(scanner.hasNext()) {
-                word = scanner.next();
+            Pattern pattern = Pattern.compile("\\b[A-Za-zА-Яа-я]+\\b");
+            Matcher matcher = pattern.matcher(scanner.toString());;
+            while(matcher.find()) {
+                word = matcher.group();
                 if(maxLengthWord.length() < word.length())
                     maxLengthWord = word;
             }
